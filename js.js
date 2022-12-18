876f0f043fc581f2698513be6fed247ec084ec80
Sergey <telmy@yandex.ru>
2022-12-18 16:19:19 +0300

    feat: update css, add js

---
+++ js.js
@@ -0,0 +1,9 @@
+const menuBtn = document.querySelector('.course');
+const nav = document.querySelector('.courses');
+
+
+menuBtn.addEventListener('click', function () {
+   nav.classList.add('active');
+
+
+})
