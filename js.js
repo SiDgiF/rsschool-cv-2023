71b6c664c7865b19e40fe182f09f7cc8b22235ca
Sergey <telmy@yandex.ru>
2023-07-05 23:35:12 +0300

    feat: update js

--- js.js
+++ js.js
@@ -1,199 +1,39 @@
+const section = document.querySelector('.section');
+
 const summaryBtn = document.querySelector('.summary__btn');
+const summaryArea = document.querySelector('#summary');
 const summary = document.querySelector('.summary');
+
 const skillsBtn = document.querySelector('.skills__btn');
 const skills = document.querySelector('.skills');
+const skillsArea = document.querySelector('#skills');
+
 const contactBtn = document.querySelector('.contact__btn');
 const contact = document.querySelector('.contact');
+const contactArea = document.querySelector('#contacts');
+
 const codeBtn = document.querySelector('.code__btn');
 const code = document.querySelector('.code');
+const codeArea = document.querySelector('#code');
+
 const coursesBtn = document.querySelector('.courses__btn');
 const courses = document.querySelector('.courses');
+const coursesArea = document.querySelector('#courses');
+
 const experienceBtn = document.querySelector('.experience__btn');
 const experience = document.querySelector('.experience');
+const experienceArea = document.querySelector('#experience');
+
 const educationBtn = document.querySelector('.education__btn');
 const education = document.querySelector('.education');
+const educationArea = document.querySelector('#education');
+
 const languagesBtn = document.querySelector('.languages__btn');
 const languages = document.querySelector('.languages');
+const languagesArea = document.querySelector('#languages');
 
 
-summaryBtn.addEventListener('click', function () {
-
-   summary.classList.add('active');
-   skills.classList.remove('active');
-   contact.classList.remove('active');
-   code.classList.remove('active');
-   courses.classList.remove('active');
-   experience.classList.remove('active');
-   education.classList.remove('active');
-   languages.classList.remove('active');
-
-})
-
-summary.addEventListener('mousemove', function () {
-
-   summary.classList.add('active');
-   skills.classList.remove('active');
-   contact.classList.remove('active');
-   code.classList.remove('active');
-   courses.classList.remove('active');
-   experience.classList.remove('active');
-   education.classList.remove('active');
-   languages.classList.remove('active');
-
-})
-
-
-skillsBtn.addEventListener('click', function () {
-
-   summary.classList.remove('active');
-   skills.classList.add('active');
-   contact.classList.remove('active');
-   code.classList.remove('active');
-   courses.classList.remove('active');
-   experience.classList.remove('active');
-   education.classList.remove('active');
-   languages.classList.remove('active');
-
-})
-
-skills.addEventListener('mousemove', function () {
-
-   summary.classList.remove('active');
-   skills.classList.add('active');
-   contact.classList.remove('active');
-   code.classList.remove('active');
-   courses.classList.remove('active');
-   experience.classList.remove('active');
-   education.classList.remove('active');
-   languages.classList.remove('active');
-
-})
-
-contactBtn.addEventListener('click', function () {
-
-   summary.classList.remove('active');
-   skills.classList.remove('active');
-   contact.classList.add('active');
-   code.classList.remove('active');
-   courses.classList.remove('active');
-   experience.classList.remove('active');
-   education.classList.remove('active');
-   languages.classList.remove('active');
-
-})
-contact.addEventListener('mousemove', function () {
-
-   summary.classList.remove('active');
-   skills.classList.remove('active');
-   contact.classList.add('active');
-   code.classList.remove('active');
-   courses.classList.remove('active');
-   experience.classList.remove('active');
-   education.classList.remove('active');
-   languages.classList.remove('active');
-
-})
-
-codeBtn.addEventListener('click', function () {
-
-   summary.classList.remove('active');
-   skills.classList.remove('active');
-   contact.classList.remove('active');
-   code.classList.add('active');
-   courses.classList.remove('active');
-   experience.classList.remove('active');
-   education.classList.remove('active');
-   languages.classList.remove('active');
-
-})
-code.addEventListener('mousemove', function () {
-
-   summary.classList.remove('active');
-   skills.classList.remove('active');
-   contact.classList.remove('active');
-   code.classList.add('active');
-   courses.classList.remove('active');
-   experience.classList.remove('active');
-   education.classList.remove('active');
-   languages.classList.remove('active');
-
-})
-
-coursesBtn.addEventListener('click', function () {
-
-   summary.classList.remove('active');
-   skills.classList.remove('active');
-   contact.classList.remove('active');
-   code.classList.remove('active');
-   courses.classList.add('active');
-   experience.classList.remove('active');
-   education.classList.remove('active');
-   languages.classList.remove('active');
-
-})
-courses.addEventListener('mousemove', function () {
-
-   summary.classList.remove('active');
-   skills.classList.remove('active');
-   contact.classList.remove('active');
-   code.classList.remove('active');
-   courses.classList.add('active');
-   experience.classList.remove('active');
-   education.classList.remove('active');
-   languages.classList.remove('active');
-
-})
-
-experienceBtn.addEventListener('click', function () {
-
-   summary.classList.remove('active');
-   skills.classList.remove('active');
-   contact.classList.remove('active');
-   code.classList.remove('active');
-   courses.classList.remove('active');
-   experience.classList.add('active');
-   education.classList.remove('active');
-   languages.classList.remove('active');
-
-})
-experience.addEventListener('mousemove', function () {
-
-   summary.classList.remove('active');
-   skills.classList.remove('active');
-   contact.classList.remove('active');
-   code.classList.remove('active');
-   courses.classList.remove('active');
-   experience.classList.add('active');
-   education.classList.remove('active');
-   languages.classList.remove('active');
-
-})
-
-educationBtn.addEventListener('click', function () {
-
-   summary.classList.remove('active');
-   skills.classList.remove('active');
-   contact.classList.remove('active');
-   code.classList.remove('active');
-   courses.classList.remove('active');
-   experience.classList.remove('active');
-   education.classList.add('active');
-   languages.classList.remove('active');
-})
-education.addEventListener('mousemove', function () {
-
-   summary.classList.remove('active');
-   skills.classList.remove('active');
-   contact.classList.remove('active');
-   code.classList.remove('active');
-   courses.classList.remove('active');
-   experience.classList.remove('active');
-   education.classList.add('active');
-   languages.classList.remove('active');
-})
-
-languagesBtn.addEventListener('click', function () {
-
+function start() {
    summary.classList.remove('active');
    skills.classList.remove('active');
    contact.classList.remove('active');
@@ -201,19 +41,43 @@ languagesBtn.addEventListener('click', function () {
    courses.classList.remove('active');
    experience.classList.remove('active');
    education.classList.remove('active');
-   languages.classList.add('active');
+   languages.classList.remove('active');
+}
 
-})
-languages.addEventListener('mousemove', function () {
+function addRemove(a) {
+   start();
+   a.classList.add('active');
+   a.scrollIntoView({ block: "center", behavior: "smooth" });
+   // Метод Element.scrollIntoView() интерфейса Element прокручивает контейнер родителя элемента так, чтобы элемент, на котором был вызван scrollIntoView(), стал виден пользователю.
+}
 
-   summary.classList.remove('active');
-   skills.classList.remove('active');
-   contact.classList.remove('active');
-   code.classList.remove('active');
-   courses.classList.remove('active');
-   experience.classList.remove('active');
-   education.classList.remove('active');
-   languages.classList.add('active');
+section.addEventListener('mousemove', function () {
+   start();
+});
 
-})
+
+summaryBtn.addEventListener('click', () => addRemove(summary));
+summaryArea.addEventListener('click', () => addRemove(summary));
+
+skillsBtn.addEventListener('click', () => addRemove(skills));
+skillsArea.addEventListener('click', () => addRemove(skills));
+
+contactBtn.addEventListener('click', () => addRemove(contact));
+contactArea.addEventListener('click', () => addRemove(contact));
+
+codeBtn.addEventListener('click', () => addRemove(code));
+codeArea.addEventListener('click', () => addRemove(code));
+
+coursesBtn.addEventListener('click', () => addRemove(courses));
+coursesArea.addEventListener('click', () => addRemove(courses));
+
+
+
+
+
+
+
+
+
+
 
